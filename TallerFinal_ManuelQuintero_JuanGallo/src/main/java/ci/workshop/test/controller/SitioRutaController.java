b7e8f2ac364978008c050902ccda7ebeae5dc13d
JuanGallooo <juanesteban3600@gmail.com>
2019-11-29 21:50:02 -0500

    Controller Sitio Ruta

---
+++ TallerFinal_ManuelQuintero_JuanGallo/src/main/java/ci/workshop/test/controller/SitioRutaController.java
@@ -0,0 +1,77 @@
+package ci.workshop.test.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import ci.workshop.test.model.Tmio1Sitio;
+import ci.workshop.test.service.TMioSitioRutaService;
+import lombok.Data;
+
+@Controller
+@Data
+public class SitioRutaController {
+TMioSitioRutaService sitioService;
+	
+	@Autowired
+	public SitioRutaController(TMioSitioRutaService sitioService){
+		this.sitioService= sitioService;
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "/customLogin";
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		return "/customLogin";
+	}
+	@GetMapping("/sitioRuta/")
+	public String index(Model model) {
+		model.addAttribute("sitiosRuta",sitioService.findAll());
+		return "/sitioRuta/index";
+	}
+	@GetMapping("/sitioRuta/add-sitioRuta")
+	public String addSitioRuta(Model model){
+		return "/sitioRuta/add-sitioRuta";
+	}
+	
+//	@PostMapping("/sitio/add-sitioRuta/")
+//	public String saveSitioRuta(@Valid @ModelAttribute("sitio") Tmio1Sitio sitio, BindingResult bindingResult, Model model) {
+//		if(bindingResult.hasErrors()) {
+//			model.addAttribute("sitioRuta",sitio);
+//			return "/sitio/add-sitioRuta";
+//		}
+//		sitioService.saveSitio(sitio);
+//		return "redirect:/sitio/";
+//	}
+//	@GetMapping("/sitioRuta/edit-sitio/{id}")
+//	public String editSitioRuta(@PathVariable("id") String id,Model model){
+//		model.addAttribute("sitioRuta",sitioService.findById(Integer.parseInt(id)));	
+//		return "/sitioRuta/edit-sitio";
+//	}
+//	@DeleteMapping("/sitioRuta/delete-sitio/{id}")
+//	public String deleteSitioRuta(@PathVariable("id") String id,Model model){
+//		sitioService.removeSitio(sitioService.findById(Integer.parseInt(id)));
+//		return "/sitio/";
+//	}
+//	
+//	@PostMapping("/sitioRuta/edit-sitio/")
+//	public String editSitioRuta(@Valid @ModelAttribute("sitio") Tmio1Sitio sitio, BindingResult bindingResult, Model model) {
+//		if(bindingResult.hasErrors()) {
+//			model.addAttribute("sitio",sitio);
+//			return "/sitio/edit-sitio";
+//		}
+//		sitioService.updateSitio(sitio);
+//		return "redirect:/sitio/";
+//	}
+}
